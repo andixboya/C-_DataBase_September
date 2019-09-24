9fad4f535d1c5dc4c303de9b73cd8aab10ef742a
andixboya <andreyanboyadzhiev@gmail.com>
2019-09-24 08:24:41 +0300

    Adding the rest of the exercises from CRUD-related operations

--- Lesson_2_CRUD.sql
+++ Lesson_2_CRUD.sql
@@ -55,3 +55,82 @@ FROM Employees AS e
 WHERE e.Salary>=50000
 ORDER BY e.Salary DESC
 GO
+--13
+SELECT TOP(5) e.FirstName,e.LastName FROM Employees AS e
+order by e.Salary DESC
+GO
+--14
+SELECT e.FirstName,e.LastName 
+FROM Employees as e
+WHERE e.DepartmentId !=4 -- or <>
+GO
+--15
+SELECT * FROM Employees AS e
+ORDER BY e.Salary DESC, 
+		 e.FirstName,
+		 e.LastName DESC,
+		 e.MiddleName
+GO
+--16
+CREATE VIEW V_EmployeeSalaries AS
+SELECT e.FirstName,e.LastName,e.Salary FROM Employees AS e
+GO
+SELECT *FROM V_EmployeeSalaries
+GO
+--17 this... was an awkward moment
+CREATE VIEW V_EmployeeNameJobTitle AS
+SELECT e.FirstName+' '+ISNULL(e.Middlename,'')+' '+e.LastName AS [Full Name],
+e.JobTitle
+FROM Employees AS e
+SELECT * FROM V_EmployeeNameJobTitle
+GO
+--18
+SELECT DISTINCT e.JobTitle FROM Employees as e
+GO
+--19
+SELECT TOP(10) * FROM Projects AS p
+ORDER BY p.StartDate,p.[Name]
+GO
+--20
+SELECT TOP(7) e.FirstName,e.LastName,e.HireDate from Employees AS e
+ORDER BY e.HireDate DESC
+GO
+--21
+GO
+UPDATE Employees 
+SET Salary*=1.12
+WHERE DepartmentID in (SELECT DISTINCT e.DepartmentID FROM Employees AS E
+INNER JOIN Departments AS d ON d.DepartmentID=e.DepartmentID 
+WHERE  d.[Name] IN ('Engineering','Tool Design','Marketing','Information Services'))
+SELECT e.Salary FROM Employees as E
+
+GO
+SELECT DISTINCT e.DepartmentID FROM Employees AS E
+INNER JOIN Departments AS d ON d.DepartmentID=e.DepartmentID 
+WHERE  d.[Name] IN ('Engineering','Tool Design','Marketing','Information Services')
+GO
+--22
+use Geography
+SELECT p.PeakName FROM Peaks AS p
+ORDER BY p.PeakName
+GO
+--23
+SELECT TOP(30) c.CountryName,c.Population FROM Countries as C
+INNER JOIN Continents as Co ON c.ContinentCode=co.ContinentCode
+WHERE co.ContinentName='Europe'
+ORDER BY c.[Population] DESC,C.CountryName
+GO
+--24 SKIP for now...
+SELECT c.CountryName,
+c.CountryCode,
+CASE WHEN c.CurrencyCode = 'EUR' THEN 'Euro'
+	  ELSE 'Not Euro' 
+	  END  AS Currency
+ From Countries AS c
+ ORDER BY c.CountryName
+
+GO
+--25
+use Diablo
+select c.Name from Characters AS c
+ORDER BY c.Name
