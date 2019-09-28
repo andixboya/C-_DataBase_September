7ef1ba9d838127b2a6a52f754f9b8b2ac6724f49
andixboya <andreyanboyadzhiev@gmail.com>
2019-09-28 08:37:13 +0300

    adding the rest of the queries related to built-in-functions

--- Lesson_3_Built_In_Functions.sql
+++ Lesson_3_Built_In_Functions.sql
@@ -70,3 +70,62 @@ FROM Peaks AS p ,
 	 WHERE RIGHT(p.PeakName,1)=LEFT(r.RiverName,1)) AS X
 	 ORDER BY X.Mix
 GO
+--14
+USE Diablo
+SELECT TOP (50) [Name],
+				--CONVERT(DATE,[Start],102) AS [Start] 
+				FORMAT([Start],'yyyy-MM-dd','en-US') AS [Start]
+	   FROM Games
+	   WHERE DATEPART(year,[Start]) IN (2011,2012) 
+	   ORDER BY [Start],[Name]
+GO
+--15
+GO
+SELECT * FROM (SELECT 
+	u.Username,
+    SUBSTRING(u.Email ,charindex('@',u.Email ,1)+1,len(u.email))AS [Email Provider] FROM Users AS u)AS f
+ORDER BY f.[Email Provider],f.Username
+GO
+--16
+SELECT u.Username,u.IpAddress FROM Users AS u
+WHERE u.IpAddress LIKE '___.1%.%.___%'
+ORDER BY u.Username
+GO
+--17
+SELECT * FROM (SELECT g.[Name] AS [Game],
+	CASE
+	WHEN DATEPART(hour,g.Start) BETWEEN 0 AND 11 THEN 'Morning'
+	WHEN DATEPART(hour,g.Start) BETWEEN 12 AND 17 THEN 'Afternoon'
+	WHEN DATEPART(hour,g.Start) BETWEEN 18 AND 23 THEN 'Evening'
+	--lol, at 23, not 24, silly!
+	END AS [Part of the Day],
+	CASE
+	WHEN g.duration BETWEEN 0 AND 3 THEN 'Extra Short'
+	WHEN g.duration BETWEEN 4 AND 6 THEN 'Short'
+	WHEN g.duration >=6 THEN 'Long'
+	WHEN g.Duration IS NULL THEN 'Extra Long'
+	END AS [Duration]
+	FROM Games AS g
+	) AS temp
+	ORDER BY temp.Game,
+			 temp.Duration,
+			 temp.[Part of the Day]
+GO
+
+--18
+--SELECT * FROM Games
+USE SoftUni
+CREATE TABLE Orders (
+ID INT PRIMARY KEY IDENTITY(1,1),
+ProductName NVARCHAR(50),
+OrderDate DATETIME
+)
+INSERT INTO Orders  VALUES
+('Butter','2016-09-19 00:00:00.000'),
+('Milk','2016-09-30 00:00:00.000'),
+('Cheese','2016-09-04 00:00:00.000')
+
+SELECT O.ProductName,o.OrderDate,
+DATEADD(DAY,3,o.OrderDate) AS [Pay Due],
+DATEADD(MONTH,1,o.OrderDate) AS [Deliver Due]
+ FROM Orders AS o
